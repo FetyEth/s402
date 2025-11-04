068e416143cf4eb812352c173502a9735c238e11
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-11-04 14:43:36 +0530

    feat: initial setup

---
+++ contracts8004/interfaces/IS8004.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IS8004 {
+    struct Agent {
+        uint256 id;
+        string _type;
+        string name;
+        string description;
+        string image;
+        string endpoint;
+        string version;
+        string[] tasks;
+        address owner;
+        uint256 reputation;
+        bool isX404;
+    }
+
+    event AddAgent(uint256 agentId);
+    event ReputationInc(uint256 agentId);
+
+    error onlyOwnerCanUpdate();
+    error OwnableInvalidOwner(address _user);
+    error NotOwner();
+}
